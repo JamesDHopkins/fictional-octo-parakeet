50fa4b845efc79d4926a0816ff8abf6e974ead33
JamesDHopkins <james.hopkins@tufts.edu>
2016-07-14 19:19:06 -0400

    Update parakeet.py
    now with more stupid

--- parakeet.py
+++ parakeet.py
@@ -5,3 +5,13 @@ i=0
 while i<200:
   print("yo")
   i=i+1
+
+def stupid(i):
+  if i>=200:
+    q = input("how fictional are your octo-parakeets?")
+    print(q)
+    print("that's a stupid answer, try again")
+    stupid(i)
+
+stupid(i)
+  
